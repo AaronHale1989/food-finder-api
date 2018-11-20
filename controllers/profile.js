42586b37cc1ebd87e14a7d16e2ec384d5cfdc276
AaronHale1989 <halea02130@gmail.com>
2018-11-20 17:29:01 -0500

    intialize server

---
+++ controllers/profile.js
@@ -0,0 +1,20 @@
+const handleProfile = (req, res, db) => {
+	const { id } = req.params;
+	let found = false;
+	db.select('*').from('users').where({
+		id: id
+	})
+		.then(user => {
+			if(user.length){
+				res.json(user);
+			} else {
+				res.status(400).json('user not found')
+			}
+			
+	})
+	
+}
+
+module.exports = {
+	handleProfile: handleProfile
+}
