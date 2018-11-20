55190f78ccab00d8012c7451dfab671c87342adf
AaronHale1989 <halea02130@gmail.com>
2018-11-20 17:51:35 -0500

    update db

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-lively-28446',
     user : 'aaronhale',
     password : '',
     database : 'foodfinder'
